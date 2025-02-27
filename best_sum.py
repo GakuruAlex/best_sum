e45024e34e5d15632a2ba799e13c03f288110461
= <=>
2025-02-27 17:04:00 +0300

    best sum with memoization

--- best_sum.py
+++ best_sum.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Dict
 def best_sum(target_sum: int, numbers: List[int])-> List[int] | None:
     """_Find the shortest list of numbers that achieve a target value from a given list_
 
@@ -23,10 +23,30 @@ def best_sum(target_sum: int, numbers: List[int])-> List[int] | None:
                 shortest_list = current
     return shortest_list
 
+def best_sum_memo(target_sum: int, numbers: List[int], memo: Dict[int, List[int]] = {0: []})-> List[int]:
+    if target_sum in memo:
+        return memo[target_sum]
+    if target_sum == 0:
+        return []
+    if target_sum < 0:
+        return None
+    shortest_list = None
+
+    for number in numbers:
+        remainder = target_sum - number
+        result = best_sum_memo(remainder, numbers, memo)
+        if result != None:
+            current = [number] + result
+            if shortest_list == None or len(shortest_list) > len(current):
+                shortest_list = current
+                memo[target_sum] = current
+
+    return shortest_list
+
 def main() -> None:
-    numbers: List[int] = [2, 3, 5]
-    target_sum: int = 8
-    best: List[int] = best_sum(target_sum, numbers)
+    numbers: List[int] = [1, 2, 5, 25]
+    target_sum: int = 100
+    best: List[int] = best_sum_memo(target_sum, numbers)
     print(f"Shortest list of int for target of {target_sum} from {numbers} is {best}")
 
 if __name__ =="__main__":
